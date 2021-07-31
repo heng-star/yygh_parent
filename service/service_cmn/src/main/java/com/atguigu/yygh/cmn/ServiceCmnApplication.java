33a10f7cd8ed694c032427dd6e31d5aa22a9cc0b
heng-star <2895695886@qq.com>
2021-07-31 23:28:50 +0800

    设置医院科室排版 查询前端接口， 2021.07.31 by xiaozhi

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})
 @EnableDiscoveryClient//启动nacos
 //@EnableFeignClients(basePackages = "com.atguigu")
-@CrossOrigin
+//@CrossOrigin
 public class ServiceCmnApplication {
     public static void main(String[] args) {
 
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/ScheduleController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/ScheduleController.java
@@ -1,38 +1,37 @@
 package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.common.result.Result;
-import com.atguigu.yygh.hosp.service.DepartmentService;
 import com.atguigu.yygh.hosp.service.ScheduleService;
 import com.atguigu.yygh.model.hosp.Schedule;
-import com.atguigu.yygh.vo.hosp.DepartmentVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
-@Api(tags = "医院科室管理")
+@Api(tags = "医院科排班管理")
 @RestController
-@RequestMapping("/admin/hosp/department")
+@RequestMapping("/admin/hosp/schedule")
 @CrossOrigin
-public class DepartmentController {
-
-    @Autowired
-    private DepartmentService departmentService;
+public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
 
-
-    //根据医院编号，查询医院所有科室列表
-    @ApiOperation(value = "查询医院所有科室列表")
-    @GetMapping("getDeptList/{hoscode}")
-    public Result getDeptList(@PathVariable String hoscode) {
-        List<DepartmentVo> list = departmentService.findDeptTree(hoscode);
-        return Result.ok(list);
+    //根据医院编号 和 科室编号 ，查询排班规则数据
+    @ApiOperation(value ="查询排班规则数据")
+    @GetMapping("getScheduleRule/{page}/{limit}/{hoscode}/{depcode}")
+    public Result getScheduleRule(@PathVariable long page,
+                                  @PathVariable long limit,
+                                  @PathVariable String hoscode,
+                                  @PathVariable String depcode) {
+        Map<String,Object> map = scheduleService.getRuleSchedule(page,limit,hoscode,depcode);
+        return Result.ok(map);
     }
 
+    //根据医院编号 、科室编号和工作日期，查询排班详细信息
     @ApiOperation(value = "查询排班详细信息")
     @GetMapping("getScheduleDetail/{hoscode}/{depcode}/{workDate}")
     public Result getScheduleDetail( @PathVariable String hoscode,
@@ -41,6 +40,4 @@ public class DepartmentController {
         List<Schedule> list = scheduleService.getDetailSchedule(hoscode,depcode,workDate);
         return Result.ok(list);
     }
-
-
 }
