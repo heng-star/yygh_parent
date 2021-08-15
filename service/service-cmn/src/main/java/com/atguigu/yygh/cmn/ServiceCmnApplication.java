458f26a32764d29a9d4557454cc9fca702f9edb9
xiaozhi_heng <2895695886@qq.com>
2021-08-15 21:38:22 +0800

    增加前端用户显示修改删除接口 2021.08.15 by xiaozhi

--- service/service-cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
+++ service/service-cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
@@ -3,16 +3,16 @@ package com.atguigu.yygh.cmn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})
 @EnableDiscoveryClient//启动nacos
-//@EnableFeignClients(basePackages = "com.atguigu")
+@EnableFeignClients(basePackages = "com.atguigu")
 //@CrossOrigin
 public class ServiceCmnApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(ServiceCmnApplication.class, args);
     }
 }
